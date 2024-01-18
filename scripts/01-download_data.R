ea98a4311c934547cccc64aa458f8a0e5ec3ed34
Diana Liu <guanzhi.liu@mail.utoronto.ca>
2024-01-18 00:24:25 +0000

    download toronto marriage data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,35 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from opendatatoronto
+# Author: Diana Liu
+# Date:
+# Contact: guanzhi.liu@mail.utoronto.ca
 
 
 #### Workspace setup ####
+# install.packages("opendatatoronto")
+# install.packages("tidyverse")
+# install.packages("dplyr")
+# comment out installations after they are done
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
+toronto_marriages <-
+  # Each package is associated with a unique id  found in the "For 
+  # Developers" tab of the relevant page from Open Data Toronto
+  # https://open.toronto.ca/dataset/marriage-licence-statistics/
+  list_package_resources("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb") |>
+  # we are only interested in the .csv data file
+filter(name == "Marriage Licence Statistics Data.csv") |>
+  # Having reduced the dataset to one row we can get the resource
+  get_resource()
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+# After downloading the data, we can save it under a descriptive name
+write_csv(
+  x = toronto_marriages,
+  file = "inputs/data/unedited_data.csv"
+) 
