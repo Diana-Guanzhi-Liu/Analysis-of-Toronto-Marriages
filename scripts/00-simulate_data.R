c79e3ece4c011fcb8f23e6f9382b5b5df74e0b2d
Diana Liu <guanzhi.liu@mail.utoronto.ca>
2024-01-18 00:35:36 +0000

    edited variable Marriages

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -20,7 +20,7 @@ simulated_marriage_data <-
       format(
         seq(as.Date("2023/1/1"), by = "month", length.out = 12), format="%B"
         ),
-    Marriage_licenses =
+    Marriages =
       round(runif(n = 12, min = 0, max = 2000))
   )
 
@@ -28,8 +28,8 @@ simulated_marriage_data
 
 
 #### Test Simulated Data ####
-simulated_marriage_data$Marriage_licenses |> min() >= 0
-simulated_marriage_data$Marriage_licenses |> max() <= 2000
-simulated_marriage_data$Marriage_licenses |> class() == 'numeric'
+simulated_marriage_data$Marriages |> min() >= 0
+simulated_marriage_data$Marriages |> max() <= 2000
+simulated_marriage_data$Marriages |> class() == 'numeric'
 simulated_marriage_data$Month |> class() == 'character'
 
